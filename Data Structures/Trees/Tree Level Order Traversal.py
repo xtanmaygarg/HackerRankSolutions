465ca98ccf545144be163f5e3c3989c8406f8808
xtanmaygarg <xtanmaygarg@gmail.com>
2020-03-17 23:06:37 +0530

    Added Trees Level Order Traversal Using Queue.

---
+++ Data Structures/Trees/Tree Level Order Traversal.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def levelOrder(root):
+    L = 1
+    i = 0
+    Queue = [root]
+    while(i < L):
+        Current = Queue[i]
+        print(Current.info, end = " ")
+        if Current.left and Current.right:
+            Queue.append(Current.left)
+            Queue.append(Current.right)
+            L += 2
+        elif Current.left:
+            Queue.append(Current.left)
+            L += 1
+        elif Current.right:
+            Queue.append(Current.right)
+            L += 1
+        else:
+            pass
+        i += 1
+
+
+tree = BinarySearchTree()
+t = int(input())
+arr = list(map(int, input().split()))
+for i in range(t):
+    tree.create(arr[i])
+levelOrder(tree.root)
