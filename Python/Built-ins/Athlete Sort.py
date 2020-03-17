5f249b02b9f6c805ebe8d96e2a5e6cd46c78e8c9
xtanmaygarg <xtanmaygarg@gmail.com>
2020-03-18 03:08:09 +0530

    Added 'Built-ins: Athlete Sort'

---
+++ Python/Built-ins/Athlete Sort.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    nm = input().split()
+    n = int(nm[0])
+    m = int(nm[1])
+    arr = []
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+    k = int(input())
+    
+    for i in sorted(arr, key = lambda X: X[k]):
+        print(*i)
