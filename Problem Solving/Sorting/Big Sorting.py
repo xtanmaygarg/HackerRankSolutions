59c829c0bf88d3f9d21d883267cb4731b5f702ba
xtanmaygarg <xtanmaygarg@gmail.com>
2020-03-22 02:11:32 +0530

    Added Problem Solving: Sorting: Big Sorting.py

---
+++ Problem Solving/Sorting/Big Sorting.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import os
+
+# Complete the bigSorting function below.
+def bigSorting(unsorted):
+    return sorted(sorted(unsorted), key = len)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    unsorted = []
+
+    for _ in range(n):
+        unsorted_item = input()
+        unsorted.append(unsorted_item)
+
+    result = bigSorting(unsorted)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
