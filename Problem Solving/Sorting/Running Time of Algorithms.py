98cae0291ad295f1ae28ae0bcd7e2530825a07ef
xtanmaygarg <xtanmaygarg@gmail.com>
2020-03-23 04:33:34 +0530

    Added Problem Solving: Sorting: Running Time

---
+++ Problem Solving/Sorting/Running Time of Algorithms.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import os
+
+# Complete the runningTime function below.
+def runningTime(l):
+    Ans = 0
+    for i in range(1, len(l)):
+        j = i-1
+        key = l[i]
+        while (j >= 0) and (l[j] > key):
+           l[j+1] = l[j]
+           Ans += 1
+           j -= 1
+        l[j+1] = key
+    return Ans
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = runningTime(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
