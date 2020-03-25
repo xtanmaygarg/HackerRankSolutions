2c342ef4dcf7d77b555940edb9bf74ac3188b31b
xtanmaygarg <xtanmaygarg@gmail.com>
2020-03-25 16:47:36 +0530

    Added Sorting Problem: Counting Sort 2

---
+++ Problem Solving/Sorting/Counting Sort 2.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingSort function below.
+def countingSort(arr):
+    Result = [0] * 100
+    for i in arr:
+        Result[i] += 1
+    Answer = []
+    for i in range(100):
+        Answer.extend([i] * Result[i])
+    return Answer
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
