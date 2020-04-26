d1c7c2efc42719d2c41368981125264d62d2d004
Tanmay Garg <xtanmaygarg@gmail.com>
2020-04-27 01:43:19 +0530

    Added 2 Problems

---
+++ Problem Solving/Strings/Beautiful Binary String.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import os
+
+def beautifulBinaryString(b):
+    return b.count('010')
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    b = input()
+
+    result = beautifulBinaryString(b)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
